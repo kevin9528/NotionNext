97ce7874823a20bfe1f2aee0c092c94aacc76808
kevin9528 <108799936+kevin9528@users.noreply.github.com>
2024-07-11 12:32:51 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -42,13 +42,7 @@ const CONFIG = {
     '🧱 团队小组发动机'
   ],
 
-  // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
