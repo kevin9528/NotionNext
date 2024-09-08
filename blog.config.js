09330c6dfd1c76382f8a8917a4cec5dc14681af0
kevin9528 <108799936+kevin9528@users.noreply.github.com>
2024-09-08 20:24:57 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,8 +26,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'kk', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.kaolakk.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '考拉kk, 博客，Web3，比特币，以太坊', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://cccweb3.com', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '小龙btc, 博客，Web3，比特币，以太坊', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
